3a8622524e6b3c38a92b78b4b6c88c5f943b56f7
HeoBayMau <hieu.trung.030197@gmail.com>
2018-11-22 14:25:25 +0700

    Update

--- Project/SneakerC2C/SneakerC2C/Areas/Customer/Controllers/SanPhamController.cs
+++ Project/SneakerC2C/SneakerC2C/Areas/Customer/Controllers/SanPhamController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Models.BusinessLogicLayer;
 using Models.Database;
 
@@ -12,6 +13,11 @@ namespace SneakerC2C.Areas.Customer.Controllers
     [Area("Customer")]
     public class SanPhamController : BaseController
     {
+        private readonly QLBanGiayContext ctx;
+        public SanPhamController(QLBanGiayContext context)
+        {
+            ctx = context;
+        }
         public IActionResult Index(string id)
         {
             string idd = id ?? "B77D9CF5-E9A2-4D31-9490-25E4E3971C61";
@@ -25,5 +31,16 @@ namespace SneakerC2C.Areas.Customer.Controllers
             ViewBag.ListSizeSanPham = listsizesanpham;
             return View(sanpham);
         }
+        //trang list san pham
+        public IActionResult List(/*string ploai*/)
+        {
+            //List<SanPham> list = ctx.SanPham.Where(x => x.PhanLoai == ploai).ToList();
+            List<SanPham> list = ctx.SanPham.Include(sp=>sp.IdTaiKhoanNavigation)
+                                            .Include(sp=>sp.IdHangSanPhamNavigation)
+                                .ToList();
+            return View(list);
+        }
+
+
     }
 }

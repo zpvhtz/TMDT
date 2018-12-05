29fecb2ef497a316a78de11a6cc8a8f76e7313a1
Hoàng Đăng Khoa <hoangdangkhoa.m9@gmail.com>
2018-12-05 17:59:30 +0700

    Update Chart at Admin/HomePage

--- Project/SneakerC2C/SneakerC2C/Areas/Webmaster/Controllers/HomeController.cs
+++ Project/SneakerC2C/SneakerC2C/Areas/Webmaster/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Models.BusinessLogicLayer;
+using Models.DTO;
+using Newtonsoft.Json;
 
 namespace SneakerC2C.Areas.Webmaster.Controllers
 {
@@ -18,6 +21,17 @@ namespace SneakerC2C.Areas.Webmaster.Controllers
             }
             
             return View();
+        }
+
+        public JsonResult GetThongKeMerchant()
+        {
+            DateTime nbd = DateTime.Parse("10/01/2018");
+            DateTime nkt = DateTime.Parse("12/10/2018");
+            HomeBUS homeBus = new HomeBUS();
+            List<DoanhThuMerchant> list = homeBus.GetDoanhThuMerchants(nbd, nkt);
+            var json = JsonConvert.SerializeObject(list);
+            var json2 = JsonConvert.DeserializeObject(json);
+            return Json(json2);
         }
     }
 }

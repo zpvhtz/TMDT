4981e8d78c82e241f2c97d6abab67bb852d9bcac
SocBayMau <tnngo.97@gmail.com>
2018-12-07 10:13:28 +0700

    Update

--- Project/SneakerC2C/Models/Database/SanPham.cs
+++ Project/SneakerC2C/Models/Database/SanPham.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Models.Database
 {
@@ -7,6 +8,8 @@ namespace Models.Database
     {
         public SanPham()
         {
+            ChiTietPhieuDat = new HashSet<ChiTietPhieuDat>();
+            ChiTietPhieuGiao = new HashSet<ChiTietPhieuGiao>();
             SizeSanPham = new HashSet<SizeSanPham>();
         }
 
@@ -17,15 +20,20 @@ namespace Models.Database
         public string Mau { get; set; }
         public Guid IdHangSanPham { get; set; }
         public string PhanLoai { get; set; }
+        //[Required(ErrorMessage ="Xin hãy nhập giá")]
+        //[Range(0.01,100000000,ErrorMessage ="Gía phải nằm trong khoảng 0,01 VND đến 100 triệu VND")]
         public double? Gia { get; set; }
         public string Hinh { get; set; }
         public string ChiTiet { get; set; }
+        [Range(1,100,ErrorMessage ="Giảm giá phải nằm trong khoảng 1 đên 100%")]
         public double? GiamGia { get; set; }
         public DateTime? NgayDang { get; set; }
         public string TinhTrang { get; set; }
 
         public HangSanPham IdHangSanPhamNavigation { get; set; }
         public TaiKhoan IdTaiKhoanNavigation { get; set; }
+        public ICollection<ChiTietPhieuDat> ChiTietPhieuDat { get; set; }
+        public ICollection<ChiTietPhieuGiao> ChiTietPhieuGiao { get; set; }
         public ICollection<SizeSanPham> SizeSanPham { get; set; }
     }
 }

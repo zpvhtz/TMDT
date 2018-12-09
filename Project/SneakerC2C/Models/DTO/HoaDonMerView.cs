1473a6b7c03c366b5bf4c7567c6c2c7fffee6d98
SocBayMau <tnngo.97@gmail.com>
2018-12-09 17:32:59 +0700

    up

---
+++ Project/SneakerC2C/Models/DTO/HoaDonMerView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.DTO
+{
+    class HoaDonMerView
+    {
+        public Guid Id { get; set; }
+        public string MaDonHang { get; set; }
+        public string TenSanPham { get; set; }
+        public string SoLuong { get; set; }
+        public string TenMer { get; set; }
+        public string TenKH { get; set; }
+        public double? TongTien { get; set; }
+        public string TinhTrang { get; set; }
+    }
+}

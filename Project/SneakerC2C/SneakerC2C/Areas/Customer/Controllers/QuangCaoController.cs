dc0273c0ba1f75d6b5eb8acb25c314507a9ea541
quachdaivy <quachdaivy7@gmail.com>
2018-12-15 13:24:09 +0700

    UPDATE LOADQUANGCAOLENTRANGCHITESTGIUMEMHIHI

---
+++ Project/SneakerC2C/SneakerC2C/Areas/Customer/Controllers/QuangCaoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Models.BusinessLogicLayer;
+using Models.Database;
+using Newtonsoft.Json;
+
+namespace SneakerC2C.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class QuangCaoController : Controller
+    {
+       public ActionResult LoadQuangCao(string mavitri)
+        {
+            QuangCaoBUS quangcao = new QuangCaoBUS();
+            QuangCao qc = new QuangCao();
+            qc = quangcao.LoadQuangCao(mavitri);
+            var json = JsonConvert.SerializeObject(qc, Formatting.Indented,
+new JsonSerializerSettings
+{
+    ReferenceLoopHandling = ReferenceLoopHandling.Serialize
+});
+            var json2 = JsonConvert.DeserializeObject(json);
+            return Json(json2);
+        }
+    }
+}
